55b48d05e59711ec5d282625383a919fb049a5db
Filipe Marques <filipi.marques@gmail.com>
2019-08-31 18:17:45 -0300

    Limpando os arquivos e fazendo as configuracoes basicas para iniciar o curso

---
+++ my-blog/src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404</h1>
+    <p>Oops.. Não tem nada por aqui! ;/</p>
+  </Layout>
+)
+
+export default NotFoundPage
