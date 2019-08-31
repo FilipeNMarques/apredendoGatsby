96d34749451abbaa1075f8ff2cddbe71dacfe9a5
Filipe Marques <filipi.marques@gmail.com>
2019-08-31 19:24:40 -0300

    Adicionando paginas e utilizando o Gatsby Link

---
+++ my-blog/src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Filipe Marques</h1>
+    <h2>Desenvolvedor Frontend</h2>
+    <p>Aqui vou falar sobre tecnologia, empreendedorismo e evolução pessoal.</p>
+  </div>
+)
+
+export default Profile
